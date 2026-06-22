c4fd76cd3ba55e7d4a154cff88226d0aefe8b011
Jayendra Khole <jayendra.khole@e-zest.in>
2026-06-22 09:50:43 +0530

    Delete School Time Table 2026 directory

--- School Time Table 2026/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.schooltimetable.buddy',
-  appName: 'Hey Buddy Bag Packer',
-  webDir: 'dist'
-};
-
-export default config;
